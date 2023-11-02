eb97de4d808d0ca85be7d73a42853cab9fd563e5
Gianissssss <gianicpos@gmail.com>
2023-11-01 22:05:10 -0300

    avance en inicio

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+function mostrarFormulario() {
+    document.getElementById("formulario").style.display = "block";
+}
+
+function ocultarFormulario() {
+    document.getElementById("formulario").style.display = "none";
+}
+
+/* menu */
+function comprar() {
+    alert("El producto ha sido agregado al carrito.");
+}
+
+function agregarFavoritos() {
+    alert("El producto ha sido agregado a tus favoritos.");
+}
