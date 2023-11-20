7117121ee14485694a6643d5e91b80760f4ba67a
pilo <pilocipo90@gmail.com>
2023-11-20 19:29:53 -0300

    img, cards, function

--- main.js
+++ main.js
@@ -16,3 +16,25 @@ function comprar() {
 function agregarFavoritos() {
     alert("El producto ha sido agregado a tus favoritos.");
 }
+
+
+function actualizarNumero(btn, cambio) {
+    // Encuentra el elemento del contador más cercano
+    var contador = btn.closest('.counter-container').querySelector('.counter');
+    var valorActual = parseInt(contador.textContent, 10);
+    
+    // Actualiza el contador con el cambio
+    var nuevoValor = valorActual + cambio;
+    nuevoValor = Math.max(0, nuevoValor); // Asegura que no sea negativo
+    contador.textContent = nuevoValor;
+}
+
+
+function agregarAlCarrito(btn) {
+    var contadorProducto = btn.closest('.counter-container').querySelector('.counter');
+    var cantidad = parseInt(contadorProducto.textContent, 10);
+    //actualiza el numero en el carrito del nav y reinicia el contador
+    var cartItemCount = document.getElementById('cartItemCount');
+    cartItemCount.textContent = parseInt(cartItemCount.textContent, 10) + cantidad;
+    contadorProducto.textContent = '0';
+}
