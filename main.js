6fd224a90b65e40cf4c32164af33a7a609cf9b33
pilo <pilocipo90@gmail.com>
2023-11-21 16:27:14 -0300

    falta agregar prod al carrito

--- main.js
+++ main.js
@@ -9,14 +9,6 @@ function ocultarFormulario() {
 }
 
 /* menu */
-function comprar() {
-    alert("El producto ha sido agregado al carrito.");
-}
-
-function agregarFavoritos() {
-    alert("El producto ha sido agregado a tus favoritos.");
-}
-
 
 function actualizarNumero(btn, cambio) {
     // Encuentra el elemento del contador más cercano
@@ -30,15 +22,59 @@ function actualizarNumero(btn, cambio) {
 }
 
 
+
 function agregarAlCarrito(btn) {
-    var contadorProducto = btn.closest('.counter-container').querySelector('.counter');
+    //elemento padre de la información del producto
+    var menuCard = btn.closest('.menu-card');
+
+    var contadorProducto = menuCard.querySelector('.counter');
     var cantidad = parseInt(contadorProducto.textContent, 10);
-    //actualiza el numero en el carrito del nav y reinicia el contador
+
+    // Actualizar el número en el carrito del navbar y reiniciar el contador
     var cartItemCount = document.getElementById('cartItemCount');
     cartItemCount.textContent = parseInt(cartItemCount.textContent, 10) + cantidad;
     contadorProducto.textContent = '0';
+
+    // Estilos y notificación
+    cartItemCount.style.backgroundColor = 'red';
+    cartItemCount.style.padding = '6px 10px';
+
+    mostrarNotificacion('Producto agregado');
+
+    // Restablecer el estilo 
+    setTimeout(function () {
+        cartItemCount.style.backgroundColor = '#fa7f01';
+        cartItemCount.style.padding = '';
+    }, 1000);
+
+  
+    
 }
-//btn-suscribe
+
+
+
+
+function mostrarNotificacion(mensaje) {
+    var notification = document.createElement('div');
+    notification.classList.add('notification');
+    notification.textContent = mensaje;
+
+    // Agrega la notificación 
+    document.body.appendChild(notification);
+
+    // Elimina la notificación 
+    setTimeout(function () {
+        notification.remove();
+    }, 2000); // 
+}
+
+
+
+
+
+
+
+//subscripcion
 
 function clearInput() {
     document.getElementById("myInput").value = "";
@@ -59,4 +95,26 @@ document.getElementById("btn-sub").addEventListener('click', function(event) {
         alert('suscrito con exito');
     }
 });
+
+
+
+
+const btncarro =  document.querySelectorAll('.menu-btn-carro');
+btncarro.forEach(btnAdd => {
+    btnAdd.addEventListener('click', addToCartClicked);
+});
+
+
+function addToCartClicked(event) {
+    const boton = event.target;
+    const item = boton.closest('.menu-card');
+    const itemTitle = item.querySelector('.item-title').textContent;
+    const itemPrice = item.querySelector('.item-price').textContent;
+    const itemImg = item.querySelector('.item-img').src;
+
+    addItemCart(itemTitle, itemPrice, itemImg);
+    
+}
+
+
 
